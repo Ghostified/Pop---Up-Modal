c054f4bbf89b4041e076f6681f6770e4195af48a
Ghostified <bransonallan@gmail.com>
2024-09-04 12:00:17 +0300

    30:15

--- scriupt.js
+++ scriupt.js
@@ -1,23 +1,49 @@
-let popup = document.getElementById("popup");
 
-function openPopup(){
-  popup.classList.add("open-popup");
+var a = document.getElementById("loginButton");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+console.log('the vars::',a,b,x,y);
+
+function login(){
+  x.style.left = "4px";
+  y.style.right= "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+
 }
-function closePopup(){
-  popup.classList.remove("open-popup");
+function register()  {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1 ;
+
 }
 
-let eyeicon = document.getElementById(eyeicon);
-let password = document.getElementById(password);
+// let popup = document.getElementById("popup");
 
-//add click on eye icon to view the password input
-eyeicon.onclick = function(){
-  if(password.type == "password"){
-    password.type = "text";
-    eyeicon.src = "./images/eye-open.png"
-  }
-  else{
-    password.type = "password";
-    eyeicon.src = "./images/eye-close.png"
-  }
-}
+// function openPopup(){
+//   popup.classList.add("open-popup");
+// }
+// function closePopup(){
+//   popup.classList.remove("open-popup");
+// }
+
+// let eyeicon = document.getElementById(eyeicon);
+// let password = document.getElementById(password);
+
+// //add click on eye icon to view the password input
+// eyeicon.onclick = function(){
+//   if(password.type == "password"){
+//     password.type = "text";
+//     eyeicon.src = "./images/eye-open.png"
+//   }
+//   else{
+//     password.type = "password";
+//     eyeicon.src = "./images/eye-close.png"
+//   }
+// }
